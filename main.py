737b30eca307ebf406396aedaa4e682b7957ad51
nandinighuge19-cmd <nandinighuge19@gmail.com>
2025-08-07 16:22:34 +0530

    Demo

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+
+dict = {
+
+    "title": "WellnessHub API",
+    "description": "Backend for AI-Powered Medical & Emotional Support Assistant",
+}
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'Message': 'Welcome to WellnessHub API'}
+
+
+
+@app.get('/chat')
+def show(para :str):
+    return {'Data':para}
+
+
+
+
+
+
+
+
